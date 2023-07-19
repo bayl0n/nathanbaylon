05d1ac5150fff93b55d91fa8613854341707f88c
Nathan Baylon <nathanbaylon@gmail.com>
2023-07-19 19:22:29 +1000

    refactor: :art: Refactor js to use modules

--- src/app.js
+++ src/app.js
@@ -1,53 +1,14 @@
-const typeSentence = document.getElementById("type-sentence");
+import { createCarousel } from "./modules/typeCarousel.js";
 
-async function writeSentence(sentence, eleRef, delay = 100) {
-    const letters = sentence.split("");
+window.onload = () => {
+    const typeSentence = document.getElementById("type-sentence");
 
-    let i = 0;
+    const carouselText = [
+        { text: "Nathan Baylon.", color: "black" },
+        { text: "a software developer.", color: "red" },
+        { text: "a content creator.", color: "blue" },
+        { text: "a boyfriend.", color: "green" },
+    ]
 
-    while (i < letters.length) {
-        await waitForMs(delay);
-        eleRef.innerText += letters[i];
-        i++;
-    }
+    createCarousel(carouselText, typeSentence);
 }
-
-async function deleteSentence(eleRef, delay = 100) {
-    const sentence = eleRef.innerHTML;
-    const letters = sentence.split("");
-
-    while (letters.length > 0) {
-        await waitForMs(delay);
-        letters.pop();
-        eleRef.innerText = letters.join("");
-    }
-}
-
-function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const carouselText = [
-    { text: "Nathan Baylon.", color: "black" },
-    { text: "a software developer.", color: "red" },
-    { text: "a content creator.", color: "blue" },
-    { text: "a boyfriend.", color: "green" },
-]
-
-async function carousel(carouselList, eleRef) {
-    let i = 0;
-
-    while(true) {
-        eleRef.style.color = carouselList[i].color;
-
-        await writeSentence(carouselList[i].text, eleRef);
-        await waitForMs(1500);
-        await deleteSentence(eleRef);
-        await waitForMs(500);
-
-        i++;
-        if (i >= carouselList.length) i = 0;
-    }
-}
-
-carousel(carouselText, typeSentence)
---
+++ src/modules/typeCarousel.js
@@ -0,0 +1,44 @@
+async function writeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+
+    let i = 0;
+
+    while (i < letters.length) {
+        await waitForMs(delay);
+        eleRef.innerText += letters[i];
+        i++;
+    }
+}
+
+async function deleteSentence(eleRef, delay = 100) {
+    const sentence = eleRef.innerHTML;
+    const letters = sentence.split("");
+
+    while (letters.length > 0) {
+        await waitForMs(delay);
+        letters.pop();
+        eleRef.innerText = letters.join("");
+    }
+}
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function createCarousel(carouselList, eleRef) {
+    let i = 0;
+
+    while(true) {
+        eleRef.style.color = carouselList[i].color;
+
+        await writeSentence(carouselList[i].text, eleRef);
+        await waitForMs(1500);
+        await deleteSentence(eleRef);
+        await waitForMs(500);
+
+        i++;
+        if (i >= carouselList.length) i = 0;
+    }
+}
+
+export { createCarousel };
