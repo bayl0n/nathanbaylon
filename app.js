7cd83db42c2bedafbc0aa1117c533de10e2a7bbe
bayl0n <nathanbaylon@gmail.com>
2023-08-01 21:22:06 +1000

    feat: :sparkles: Add preloader and adjust BEM formatting in CSS preloader

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ import { writeSentence, deleteSentence, waitForMs, createCarousel } from "./modu
 
 // Don't forget to remove the async once ugly code is removed
 window.onload = async () => {
+
+    const preloader = document.querySelector(".preloader");
+    const mainContent = document.querySelector(".main-content");
+    const loadTimeOffset = 2000;
+
+    setTimeout(() => {
+        preloader.style.opacity = "0";
+        mainContent.style.display = "block";
+    }, loadTimeOffset);
+
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, loadTimeOffset + 500);
+
+
     const typeSentence = document.getElementById("type-sentence");
     const ellipses = document.getElementById("ellipses");
 
     const carouselText = [
         { text: "Nathan Baylon.", color: "#000000" },
-        { text: "a software dev.", color: "#c21e56" },
+        { text: "a software developer.", color: "#C21E56" },
         { text: "a problem solver.", color: "#6495ED" },
     ]
 
-    createCarousel(carouselText, typeSentence);
+    setTimeout(() => {
+        createCarousel(carouselText, typeSentence);
+    }, loadTimeOffset + 1000);
 
     // Really ugly code but leaving this here temporarily
     while(true) {
